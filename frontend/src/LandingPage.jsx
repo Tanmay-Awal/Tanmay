363eaff43c87a15492270068a707040c3fe7fd61
Tanmay-Awal <sgs.tanmay.9feb.9@gmail.com>
2025-04-24 17:10:57 +0530

    Updated LandingPage.jsx

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./LandingPage.css"; // create this CSS file
+import "./LandingPage.css"; 
 
 const LandingPage = () => {
   return (
